a9a76528da5fa45ff6587eb0690ec4e6cb13f251
Антон Мулобаев <devnull@localhost>
2015-04-21 09:18:07 +0500

    Added first views, packages

---
+++ GmailClient/Controllers/MailController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GmailClient.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult Index()
+        {
+            return RedirectToAction("Inbox");
+        }
+
+        public ActionResult Inbox()
+        {
+            return View();
+        }
+    }
+}
--- GmailClient/Global.asax.cs
+++ GmailClient/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using System.Web.Optimization;
 
 namespace GmailClient
 {
@@ -18,6 +19,7 @@ namespace GmailClient
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
