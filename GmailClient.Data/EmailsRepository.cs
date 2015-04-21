aee7ddb2164f89150a9729a7089b5de3ec94d04d
Антон Мулобаев <devnull@localhost>
2015-04-21 10:22:30 +0500

    Added nuget restore packages, assemblies for models and data layer, fake data for inbox

---
+++ GmailClient.Data/EmailsRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GmailClient.Model;
+
+namespace GmailClient.Data
+{
+    public class EmailsRepository
+    {
+        private static readonly List<Email> Container;
+
+        static EmailsRepository()
+        {
+            Container = new List<Email>
+            {
+                new Email {Subject = "Hello, I'm first message", Message = "First message body"},
+                new Email {Subject = "Hello, I'm second message", Message = "Second message body"}
+            };
+        }
+
+        public IEnumerable<Email> GetAll()
+        {
+            return Container;
+        }
+
+    }
+}
